f09188198dcbcfa753c801168997f9104f019e2d
April Kwong <april@functioneight.com>
2018-08-17 11:54:44 +0100

    WebMaps with Python and Folium

---
+++ map1.py
@@ -0,0 +1,33 @@
+import folium
+import pandas
+
+data = pandas.read_csv("Volcanoes.txt")
+lat = list(data["LAT"])
+lon = list(data["LON"])
+elev = list(data["ELEV"])
+
+def color_producer(elevation):
+    if elevation < 1000:
+        return 'green'
+    elif 1000 <=elevation < 3000:
+        return 'orange'
+    else:
+        return 'red'
+
+
+map = folium.Map(location=[38.58,-99], zoom_start=6, tiles = "Mapbox Bright")
+fg= folium.FeatureGroup(name="My Map")
+
+
+
+for lt,ln,el in zip(lat,lon,elev):
+
+    fg.add_child(folium.Marker(location=[lt,ln], popup =str(el)+" m", icon= folium.Icon(color=color_producer(el))))
+#fg.add_child(folium.CircleMarker(location=[lt,ln], popup =str(el)+" m", fill_color=color_producer(el), radius=6, color='grey', fill_opacity=0.8))
+
+
+fg.add_child(folium.GeoJson(data=open("world.json" , "r" , encoding= "utf-8-sig").read(),style_function=lambda x:{'fillColor':'yellow'}))
+
+
+map.add_child(fg)
+map.save("Map1.html")
